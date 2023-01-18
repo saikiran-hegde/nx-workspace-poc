e2d083682d43ea4a4aac83b108b54c63ab5521ab
saikiran-hegde <saikiran.hegde@zeotap.com>
2023-01-18 16:52:44 +0530

    Initial commit

---
+++ apps/user-listing/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-workspace-poc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'user-listing';
+}
